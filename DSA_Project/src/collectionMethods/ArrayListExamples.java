f63b67b6966081f08043251bbf2dccdac9ca6132
Niraj <niraj.ahergames999@gmail.com>
2024-11-09 13:41:57 +0530

    Adding arraylist and hashmap code

--- DSA_Project/src/collectionMethods/ArrayListExamples.java
+++ DSA_Project/src/collectionMethods/ArrayListExamples.java
@@ -1,10 +1,27 @@
 package collectionMethods;
 
+import java.util.*;
+
 public class ArrayListExamples {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("ArrayListExample");
+		
+		List<Integer> myList=new ArrayList<>();
+		myList.add(1);
+		myList.add(2);
+		myList.add(3);
+		myList.add(4);
+		
+		
+		for(int i : myList)
+		{
+			System.out.println(i);
+		}
+		
+		
+		
 
 	}
 
---
+++ DSA_Project/src/collectionMethods/HashMapExample.java
@@ -0,0 +1,19 @@
+package collectionMethods;
+
+import java.util.*;
+
+public class HashMapExample {
+
+	public static void main (String args[])
+	{
+		Map<Integer,Integer> myMap=new HashMap<>();
+		
+		myMap.put(null, null);
+		myMap.put(1, 1);
+		myMap.put(2, 4);
+		myMap.put(3, 9);
+		
+		System.out.print(myMap.get(2));
+		
+	}
+}
