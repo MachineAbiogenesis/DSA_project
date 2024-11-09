9c6741f10a79f2e8497f93eefeaef249a8ffa7fd
Niraj <niraj.ahergames999@gmail.com>
2024-11-09 12:59:24 +0530

    creating project

---
+++ DSA_Project/src/collectionMethods/ArrayListExamples.java
@@ -0,0 +1,11 @@
+package collectionMethods;
+
+public class ArrayListExamples {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ArrayListExample");
+
+	}
+
+}
